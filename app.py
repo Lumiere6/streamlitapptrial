097b0c244a3113aac8a18dc83857a1ee66c3fbc5
Vyshnavi <121675751+Lumiere6@users.noreply.github.com>
2024-06-13 19:23:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,10 +78,10 @@ if (selected == 'Audio Phishing'):
     tokenized_transcripts=tokens.texts_to_sequences(cleaned_transcripts)
     X = pad_sequences(tokenized_transcripts,maxlen=100,padding='post')
     pred=audio_phish_model.predict(X)
-    average_prediction = np.mean(pred,axis=0)
+    #average_prediction = np.mean(pred,axis=0)
     
     class_names=['Legitimate','Phishing']
-    prediction= "The text is predicted to be: "+ class_names[np.argmax(average_prediction)]
+    prediction= "The text is predicted to be: "+ class_names[np.argmax(pred)]
 if st.button("results"):
   st.success(prediction)
 
